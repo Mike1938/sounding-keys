ed8d42671a0543ede247b8cf6a05f0b3acfad903
Mike1938 <mmaldonadosan1938@gmail.com>
2022-05-16 10:18:18 -0400

    Worked on the landing page and the content there added photos to there and created a signup for news letter option with and other sections

--- src/Home.js
+++ src/Home.js
@@ -21,8 +21,26 @@ const Home = ()=>{
             <div className="productInfo">
                 <InfoBlock
                 title="Bringing portability and music together"
-                imgONe=""
+                imgOne="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80"
+                imgTwo="https://images.unsplash.com/photo-1512733596533-7b00ccf8ebaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80"
+                imgThree="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80"
                 />
+            </div>
+            <div className="reveal">
+                <InfoBlock
+                    title="Closer and Closer to the reveal date"
+                    imgOne="../images/Post1.png"
+                    imgTwo="../images/Post2.png"
+                    imgThree="../images/sketchDesign.png"
+                />
+            </div>
+            <div className="newsLetter">
+                <form>
+                    <p className="formTitle">Signup for newsLetter</p>
+                    <label className="labelStyles" for="signEmail">Email</label>
+                    <input id="signEmail" type="email"/>
+                    <button>Signup</button>
+                </form>
             </div>
             <Footer/>
         </div>
